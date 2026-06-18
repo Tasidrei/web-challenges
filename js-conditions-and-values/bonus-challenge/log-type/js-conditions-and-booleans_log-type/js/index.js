0ddf64cfe975e53229857e893f17e18c71b13cc8
Harry Bruns <harybruns1631@hotmail.com>
2026-06-18 22:13:14 +0200

    This task is not done yet. I will revise later on.

---
+++ js-conditions-and-values/bonus-challenge/log-type/js-conditions-and-booleans_log-type/js/index.js
@@ -0,0 +1,9 @@
+// Change this value to test different data types
+const data = "Hello World";
+console.log ("Hello World")
+
+// 1. Declare the variable named data (assign any value to test)
+const data = "Hello World"; 
+
+// 2. Write the switch statement that evaluates complex types and typeof strings
+
