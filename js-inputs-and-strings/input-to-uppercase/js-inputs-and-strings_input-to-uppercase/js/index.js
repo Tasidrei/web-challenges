a10639488f3f5aec9836eac0c01a1851f11da215
Harry Bruns <harybruns1631@hotmail.com>
2026-06-23 00:26:45 +0200

    I have touched this exercise, but will come back to this again.

--- js-inputs-and-strings/input-to-uppercase/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js-inputs-and-strings/input-to-uppercase/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,17 @@
 console.clear();
+
+const firstInput = document.querySelector ('[data-js="first-input"]');
+const secondInput = document.querySelector ('[data-js="second-input"]');
+const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
+const lowewrCaseButton = document.querySelector('[data-js="button-lowercase"]');
+
+
+uppercaseButton.addEventListener ('click', () => {
+    firstInput.value = firstInput.value.toUpperCase();
+    secondInput.value = secondInput.value.toUpperCase();
+})
+
+lowewrCaseButton.addEventListener ('click', () => {
+    firstInput.value = firstInput.value.lowewrCaseButton();
+    secondInput.value = secondInput.value.lowewrCaseButton ();
+})
