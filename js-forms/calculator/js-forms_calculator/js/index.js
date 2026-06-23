447a3936becd071b2578d023ebe0a22778b04fb3
Harry Bruns <harybruns1631@hotmail.com>
2026-06-24 00:15:16 +0200

    This is done, but needs to be fixed.

--- js-forms/calculator/js-forms_calculator/js/index.js
+++ js-forms/calculator/js-forms_calculator/js/index.js
@@ -26,7 +26,30 @@ form.addEventListener("submit", (event) => {
 
   // --v-- write your code here --v--
 
-  // --^-- write your code here --^--
+ //  get input values
+const numberA = Number(form.elements.numberA.value);
+const numberB = Number(form.elements.numberB.value);
+const operator = form.elements.operator.value;
 
+let result;
+
+  // --^-- write your code here --^--
+  // performing the correct operation
+  switch (operator) {
+    case "addition":
+      result = add(numberA, numberB);
+      break;
+    case "subtraction":
+      result = subtract(numberA, numberB);
+      break;
+    case "multiplication":
+      result = multiply(numberA, numberB);
+      break;
+    case "division":
+      result = divide(numberA, numberB);
+      break;
+    default:
+      result = "Invalid operator";
+  }
   resultOutput.textContent = result;
 });
