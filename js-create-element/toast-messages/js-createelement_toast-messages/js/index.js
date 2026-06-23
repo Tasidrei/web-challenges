68890791688cc7c9b7e8dbbf26ba48abad55dbd9
Harry Bruns <harybruns1631@hotmail.com>
2026-06-23 16:39:58 +0200

    this task is now done.

--- js-create-element/toast-messages/js-createelement_toast-messages/js/index.js
+++ js-create-element/toast-messages/js-createelement_toast-messages/js/index.js
@@ -6,8 +6,18 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const newToastMessage = document.createElement('li');
+  newToastMessage.classList.add ('toast-container__message');
+  newToastMessage.textContent = "I'm a toast message.";
+  toastContainer.append(newToastMessage)
+  console.log(newToastMessage);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  const clearMessage = document.createElement('li');
+  clearMessage.classList.remove ('toast-container__message');
+  clearMessage.textContent = "I'm a toast message.";
+  toastContainer.remove(clearMessage)
+  console.log(clearMessage);
 });
