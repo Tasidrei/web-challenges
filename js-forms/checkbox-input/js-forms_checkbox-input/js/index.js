b7e69b24618a9e887d03b054deda61c1d9ea7b24
Harry Bruns <harybruns1631@hotmail.com>
2026-06-23 23:17:51 +0200

    another task done.

--- js-forms/checkbox-input/js-forms_checkbox-input/js/index.js
+++ js-forms/checkbox-input/js-forms_checkbox-input/js/index.js
@@ -16,8 +16,30 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+// hide error initially before it is displayed
+
+hideTosError();
+
+// use event listner and the change event on the checkbox
+
+tosCheckbox.addEventListener("change", () => {
+  if (tosCheckbox.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
+});
 
   // --^-- write your code here --^--
+
+// if checkbox not checked, show error and stop the submission action
+if (!tosCheckbox.checked) {
+  showTosError();
+  return;
+}
+  // --^-- write your code here --^--
+  // if checkbox is checked, show success and hide the error message
+hideTosError();
 
   // eslint-disable-next-line no-alert
   alert("Form submitted");
