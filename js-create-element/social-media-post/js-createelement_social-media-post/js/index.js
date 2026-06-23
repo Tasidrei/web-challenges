2d8e6c3327ff64c32f45039e4b8817153a77ac61
Harry Bruns <harybruns1631@hotmail.com>
2026-06-23 20:03:59 +0200

    recommit this task again.

--- js-create-element/social-media-post/js-createelement_social-media-post/js/index.js
+++ js-create-element/social-media-post/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,42 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+// create variables
+const newPost = document.createElement('article');
+const newContent = document.createElement('p');
+const newFooter = document.createElement('footer');
+const newUserName = document.createElement('span');
+const newButton = document.createElement('button');
+
+// configure or assign classes
+newPost.classList.add('post');
+newContent.classList.add('post_content');
+newFooter.classList.add('post_footer');
+newUserName.classList.add('post_username');
+newButton.classList.add('post_button');
+
+// add text content
+newContent.textContent = "This is a new post. Like, comment and share. Thank you.";
+newUserName.textContent = "@username";
+newButton.textContent = "♥ Like";
+
+// fix the "like" button position
+newFooter.style.display = "flex";
+newFooter.style.justifyContent = "space-between";
+newFooter.style.alignItems = "center";
+
+// add data attribute for JS selection
+newButton.setAttribute("data-js", "like-button");
+
+// appending and correct nesting (element.append(childElement1, childElement2);)
+newFooter.append(newUserName, newButton);
+
+newPost.append(newContent, newFooter);
+//newContent.append(newContent);
+//newUserName.append(newUserName);
+//newButton.append(newButton);
+document.body.append(newPost);
+
+// add event listner to the new like button
+newButton.addEventListener("click", handleLikeButtonClick);
