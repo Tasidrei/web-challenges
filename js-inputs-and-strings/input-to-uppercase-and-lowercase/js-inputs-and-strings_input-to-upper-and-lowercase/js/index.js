ec546afbf4f40ad1eec417050f9d2394e745781a
Harry Bruns <harybruns1631@hotmail.com>
2026-06-24 11:06:00 +0200

    await feedback from the coaches.

--- js-inputs-and-strings/input-to-uppercase-and-lowercase/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
+++ js-inputs-and-strings/input-to-uppercase-and-lowercase/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
@@ -1,8 +1,16 @@
 console.clear();
-
-const input = document.querySelector('[data-js="first-input"]');
+// Uppercase logic first input - first button
+const firstInput = document.querySelector('[data-js="first-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 
 uppercaseButton.addEventListener("click", () => {
-  input.value = input.value.toUpperCase();
+  firstInput.value = firstInput.value.toUpperCase();
+});
+
+// Lowercase logic second input - second button
+const secondInput = document.querySelector('[data-js="second-input"]');
+const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+
+lowercaseButton.addEventListener("click", () => {
+  secondInput.value = secondInput.value.toLowercase();
 });
