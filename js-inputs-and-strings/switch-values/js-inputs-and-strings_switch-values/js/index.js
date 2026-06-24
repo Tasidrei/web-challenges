72e28be2d3bc9fabd5a78ae96f4a3e7644759c72
Harry Bruns <harybruns1631@hotmail.com>
2026-06-24 12:26:20 +0200

    task done and moving on to the next one. But will review this aǵain.

--- js-inputs-and-strings/switch-values/js-inputs-and-strings_switch-values/js/index.js
+++ js-inputs-and-strings/switch-values/js-inputs-and-strings_switch-values/js/index.js
@@ -4,10 +4,26 @@ const firstInput = document.querySelector('[data-js="first-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
 
+// assigning the second input to a variable
+const secondInput = document.querySelector('[data-js="second-input"]');
+
+// assigning the switch input button to a variable
+const switchButton = document.querySelector('[data-js="button-switch"]');
+
+// Uppercase both inputs actions
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
+  secondInput.value = secondInput.value.lowercaseButton();
 });
 
+// Lowercase both inputs actions
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+  secondInput.value = secondInput.value.lowercaseButton();
+});
+
+switchButton.addEventListener("click", () => {
+  const temp = firstInput.value;
+  firstInput.value = secondInput.value;
+  secondInput.value =temp;
 });
