602a17577aade5a09f7f5900bcc3c756157c3686
Harry Bruns <harybruns1631@hotmail.com>
2026-07-02 23:04:04 +0200

    for now it is done. Will come back to this one for the tests.

--- js-array-methods/employees/js-array-methods_employees/js/index.js
+++ js-array-methods/employees/js-array-methods_employees/js/index.js
@@ -1,4 +1,5 @@
 import { employees } from "../utils/db.js";
+console.log(employees);
 
 // Exercise 1:
 // This function receives an array of employees and a last name.
@@ -6,20 +7,20 @@ import { employees } from "../utils/db.js";
 // Check the browser preview and conduct some tests to ensure it works correctly.
 
 function findByLastName(employees, lastName) {
-  return null;
+  return employees.find((employee) => employee.lastName === lastName) || null;
 
   // Exercise 2:
   // The same as above, but now you are searching by the 'id'.
 }
 function findById(employees, id) {
-  return null;
+  return employees.find((employee) => employee.id === id) || null;
 }
 
 // Exercise 3:
 // This time you want to find an employee who lives in a city that includes a specific substring.
 
 function findByCitySubString(employees, string) {
-  return null;
+  return employees.find((employee) => employee.city.includes(string)) || null;
 }
 
 // Bonus:
@@ -27,7 +28,7 @@ function findByCitySubString(employees, string) {
 // AND who is older than a specific age
 
 function findByProfessionSubStringAndAge(employees, string, age) {
-  return null;
+  return employees.find((employee) => employee.profession.includes(string) && employee.age > age) || null;
 }
 
 // ------------------------------------------------------------------------------
