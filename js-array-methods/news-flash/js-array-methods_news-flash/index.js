0320098f1f783450f375f9392d2c2c7bd1ed493b
Harry Bruns <harybruns1631@hotmail.com>
2026-07-05 12:50:27 +0200

    same topic learned.

--- js-array-methods/news-flash/js-array-methods_news-flash/index.js
+++ js-array-methods/news-flash/js-array-methods_news-flash/index.js
@@ -8,14 +8,15 @@ const container = document.querySelector('[data-js="card-container"]');
 const filteredNews = news.filter(() => {
   return true;
 });
+const filteredNews = news.filter((card) => card.categories.includes("politics"));
 
 // Part 2 - start here
 const sortedNews = filteredNews;
 
-sortedNews.forEach((news) => {
-  const cardElement = Card(news);
-  container.append(cardElement);
+const sortedNews = filteredNews.toSorted((a, b) => {
+  return a.body.length - b.body.length;
 });
+
 
 // Check your filter and sorting order here. Have a look at the console to see if you're right or wrong.
 checkFilteredNews(filteredNews);
