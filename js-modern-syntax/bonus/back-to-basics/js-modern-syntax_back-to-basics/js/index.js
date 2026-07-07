914620e87afcedf86f8e3cae323d04ef61ebf857
Harry Bruns <harybruns1631@hotmail.com>
2026-07-07 19:00:52 +0200

    interesting part of javascript, which I need to revist again.

--- js-modern-syntax/bonus/back-to-basics/js-modern-syntax_back-to-basics/js/index.js
+++ js-modern-syntax/bonus/back-to-basics/js-modern-syntax_back-to-basics/js/index.js
@@ -3,8 +3,15 @@ Now that you've practiced destructuring, default parameters, and the spread oper
 try reversing the challenge by rewriting this code without using these modern features.
 */
 
+// Task 1
 export const getNameAndCountry = ({ name, country }) => [name, country];
 
+// Old way of writing the function without destructuring, default parameters, and spread operator
+export function getNameAndCountry(person) {
+  return [person.name, person.country];
+}
+
+// Task 2
 export const getRelocatedCity = (
   city1,
   city2 = { name: "Berlin", country: "Germany" }
@@ -15,3 +22,25 @@ export const getRelocatedCity = (
     country,
   };
 };
+
+// Old way of writing the function without destructuring, default parameters, and spread operator
+export function getRelocatedCity(city1, city2) {
+  // Default parameter replacement
+  if (!city2) {
+    city2 = { name: "Berlin", country: "Germany" };
+  }
+
+  // Replace destructuring
+  const nameAndCountry = getNameAndCountry(city2);
+  const country = nameAndCountry[1];
+
+  // Replace spread operator
+  const relocatedCity = {};
+  for (const key in city1) {
+    relocatedCity[key] = city1[key];
+  }
+
+  relocatedCity.country = country;
+
+  return relocatedCity;
+}
