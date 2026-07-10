e8ac73736ee8bb2aa8fd3d49ffffaf5aa41cdba4
Harry Bruns <harybruns1631@hotmail.com>
2026-07-10 18:32:17 +0200

    second exercise on react.js basic is done.

---
+++ react-basics/hello-world-article/react-basics_hello-world-article/src/App.jsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return <h1>Replace me with your Component!</h1>;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello World</h1>
+      <h2>Welcome to My Article</h2>
+      <p>This is a simple article component.</p>
+    </article>
+  );
+}
