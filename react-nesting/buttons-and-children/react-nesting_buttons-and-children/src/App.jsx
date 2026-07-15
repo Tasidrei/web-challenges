9e67fbcbc695c6e9629f8693a1dfb7a89e47d7d0
Harry Bruns <harybruns1631@hotmail.com>
2026-07-15 17:40:41 +0200

    first task about react nesting using children elements.

--- react-nesting/buttons-and-children/react-nesting_buttons-and-children/src/App.jsx
+++ react-nesting/buttons-and-children/react-nesting_buttons-and-children/src/App.jsx
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button color="#ff6b6b"><span>🔥</span>Click with emotion</Button>
+      <Button color="#ff4757"><span>❌</span><strong>Don't click</strong> me</Button>
+      <Button color="#ffa502"><span>⚠️</span>Avoid clicking</Button>
+      <Button color="#2ed573"><span>✅</span>Last button</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({children, color}) {
   return (
-    <button className="button" type="button">
-      Click me!
+    <button className="button" type="button" style={{ backgroundColor: color }}>
+      {children}
     </button>
   );
 }
