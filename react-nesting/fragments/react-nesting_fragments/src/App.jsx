c7eed6064555545834e7b6d3c8dbbe584eca4e92
Harry Bruns <harybruns1631@hotmail.com>
2026-07-15 17:59:45 +0200

    tackling the second exercise using the fragment tag.

--- react-nesting/fragments/react-nesting_fragments/src/App.jsx
+++ react-nesting/fragments/react-nesting_fragments/src/App.jsx
@@ -4,10 +4,20 @@ import Box from "./components/Box";
 
 export default function App() {
   return (
-    <main className="flex-container">
+   <div className="flex-container">
       <Box color="#007bff" />
       <Box color="#fc3" />
       <Box color="#ff3333" />
-    </main>
+    </div>
+  );
+}
+
+function Boxes() {
+  return (
+    <>
+      <Box color="#007bff" />
+      <Box color="#fc3" />
+      <Box color="#ff3333" />
+    </>
   );
 }
