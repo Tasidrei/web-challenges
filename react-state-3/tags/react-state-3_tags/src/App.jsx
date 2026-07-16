7227b1cd06bb44223f1e538d3746896abcb357bc
Harry Bruns <harybruns1631@hotmail.com>
2026-07-16 21:01:05 +0200

    task two was more about understanding the state function such as passing down to children and lifting up to parents.

--- react-state-3/tags/react-state-3_tags/src/App.jsx
+++ react-state-3/tags/react-state-3_tags/src/App.jsx
@@ -6,10 +6,19 @@ import List from "./components/List";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddTag(newTag) {
+    setTags([...tags, newTag]);
+  }
+
+  function handleRemoveTag(tagToRemove) {
+    setTags(tags.filter((tag) => tag !== tagToRemove));
+  } 
+  
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onRemoveTag={handleRemoveTag} />
     </main>
   );
 }
+
